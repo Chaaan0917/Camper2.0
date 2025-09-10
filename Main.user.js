1097774ca5dc1b0ef023a3a245872a66b3369b83
HideOut <96813893+Chaaan0917@users.noreply.github.com>
2025-09-10 14:24:28 +0800

    Create Main.user.js

---
+++ Main.user.js
@@ -0,0 +1,33 @@
+// ==UserScript==
+// @name         Camper CAT
+// @namespace    http://tampermonkey.net/
+// @version      3.5
+// @author       CAMPER
+// @description  lootlink, work.ink, & lockr --supported
+// @match        *://loot-link.com/s*
+// @match        *://loot-links.com/s*
+// @match        *://lootlink.org/s*
+// @match        *://lootlinks.co/s*
+// @match        *://lootdest.info/s*
+// @match        *://lootdest.org/s*
+// @match        *://lootdest.com/s*
+// @match        *://links-loot.com/s*
+// @match        *://linksloot.net/s*
+// @match        *://work.ink/*
+// @match        *://*.work.ink/*
+// @match        *://lockr.so/*
+// @match        *://blox-script.com/get-key*
+// @match        *://deltaios-executor.com/ads.html*
+// @include      *://deltaios-executor.com/ads.html*
+// @include      *loot*
+// @include      *work.ink*
+// @include      *blox-script.com/gey-key*
+// @include      *lockr.so*
+// @require      https://github.com/Chaaan0917/Camper2.0/raw/main/Camper.user.js
+// @require      https://github.com/Chaaan0917/Camper2.0/raw/main/Camper.user.js
+// @run-at       document-idle
+// @icon         https://i.pinimg.com/736x/02/72/16/02721647f507c80673b1b8ac20a82de3.jpg
+// @grant        none
+// ==/UserScript==
+
+
