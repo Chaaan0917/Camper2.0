39851092359d914f0d8a319a378cdbd0b5476796
HideOut <96813893+Chaaan0917@users.noreply.github.com>
2025-09-10 14:43:46 +0800

    Update Main.user.js

--- Main.user.js
+++ Main.user.js
@@ -17,13 +17,10 @@
 // @match        *://*.work.ink/*
 // @match        *://lockr.so/*
 // @match        *://blox-script.com/get-key*
-// @match        *://deltaios-executor.com/ads.html*
-// @include      *://deltaios-executor.com/ads.html*
 // @include      *loot*
 // @include      *work.ink*
 // @include      *blox-script.com/gey-key*
 // @include      *lockr.so*
-// @require      https://github.com/Chaaan0917/Camper2.0/raw/main/Camper.user.js
 // @require      https://github.com/Chaaan0917/Camper2.0/raw/main/Camper.user.js
 // @run-at       document-idle
 // @icon         https://i.pinimg.com/736x/02/72/16/02721647f507c80673b1b8ac20a82de3.jpg
