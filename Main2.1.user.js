1575b8bfd216c65a76a7d752b07aa4a38e88ccab
HideOut <96813893+Chaaan0917@users.noreply.github.com>
2025-09-27 21:05:03 +0800

    Create Main2.1.user.js

---
+++ Main2.1.user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         Camper Rinku Cat Test
+// @namespace    http://tampermonkey.net/
+// @author       Camper
+// @version      1.0
+// @description  Automate rinku(not bypass)
+// @match        *://*/*
+// @require      https://github.com/Chaaan0917/Camper2.0/raw/refs/heads/main/Rinku.Cat.user.js
+// @icon         https://tse2.mm.bing.net/th/id/OIP.L5yCQIs4iE6WKHXZs8-j1QHaDt?rs=1&pid=ImgDetMain&o=7&rm=3
+// @grant        none
+// ==/UserScript==
