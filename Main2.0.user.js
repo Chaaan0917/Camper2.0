035f80f78db07ef928a261450e3376eb85f61885
HideOut <96813893+Chaaan0917@users.noreply.github.com>
2025-10-04 20:56:48 +0800

    Update Main2.0.user.js

--- Main2.0.user.js
+++ Main2.0.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Camper Wonk.Ink Cat
 // @namespace    http://tampermonkey.net/
-// @version      1.6
+// @version      1.7
 // @author       CAMPER
 // @description  Automate Work.ink (not the best)
 // @match        *://work.ink/*
