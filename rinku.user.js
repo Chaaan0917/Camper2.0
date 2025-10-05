270c0600586aecb4584be8361753c248113f49b8
HideOut <96813893+Chaaan0917@users.noreply.github.com>
2025-10-05 22:22:15 +0800

    Update rinku.user.js

--- rinku.user.js
+++ rinku.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Camper Rinku Cat
 // @namespace    http://tampermonkey.net/
-// @version      1.4
+// @version      1.5
 // @description  rinku automate (no bypass)
 // @author       Camper
 // @match        *://*/*
