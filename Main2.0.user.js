00fb7e0cdae8a9d535dff0a1c056146a7025aa3a
HideOut <96813893+Chaaan0917@users.noreply.github.com>
2025-10-31 21:40:15 +0800

    Update Main2.0.user.js
    fixed lootlink (delta)

--- Main2.0.user.js
+++ Main2.0.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Camper Wonk.Ink Cat
 // @namespace    http://tampermonkey.net/
-// @version      3.4
+// @version      3.5
 // @author       CAMPER
 // @description  Work.ink bypass based on IHaxU
 // @match        *://work.ink/*
