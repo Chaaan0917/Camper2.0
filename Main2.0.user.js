81ed402b93d2bcc0b14dce104aed1b4a2e1c4a46
HideOut <96813893+Chaaan0917@users.noreply.github.com>
2025-11-12 09:47:06 +0800

    Update Main2.0.user.js
    UPDATE MANY SUPPORTS ADDED

--- Main2.0.user.js
+++ Main2.0.user.js
@@ -1,11 +1,10 @@
 // ==UserScript==
 // @name         Camper Wonk.Ink Cat
 // @namespace    http://tampermonkey.net/
-// @version      3.9
+// @version      4.0
 // @author       CAMPER
 // @description  Work.ink bypass based on IHaxU
 // @match        *://work.ink/*
-// @match        *://*.work.ink/*
 // @match        *://loot-link.com/s?*
 // @match        *://loot-links.com/s?*
 // @match        *://lootlink.org/s?*
@@ -19,15 +18,51 @@
 // @match        *://link-unlock*
 // @match        *://linkvertise.com/*/*
 // @match        *://key.volcano.wtf/*
+// @match        *://bit.do/*
+// @match        *://bit.ly/*
+// @match        *://blox-script.com/*
+// @match        *://cl.gy/*
+// @match        *://goo.gl/*
+// @match        *://is.gd/*
+// @match        *://link-hub.net/*
+// @match        *://link4m.com/*
+// @match        *://mediafire.com/*
+// @match        *://rebrand.ly/*
+// @match        *://rkns.link/*
+// @match        *://sub4unlock.com/*
+// @match        *://t.co/*
+// @match        *://t.ly/*
+// @match        *://tiny.cc/*
+// @match        *://tinylink.onl/*
+// @match        *://tinyurl.com/*
+// @match        *://v.gd/*
+// @match        *://socialwolvez.com/*
+// @match        *://adfoc.us/*
+// @match        *://sub2get.com/*
+// @match        *://sub2unlock.com/*
+// @match        *://sub2unlock.net/*
+// @match        *://mboost.me/*
+// @match        *://deltaios-executor.com/*
+// @match        *://krnl-ios.com/*
+// @match        *://auth.platoboost.app/*
+// @match        *://auth.platoboost.net/*
+// @match        *://best-links.org/*
+// @match        *://pandadevelopment.net/*
+// @match        *://krnl.cat/*
+// @match        *://lockr.so/*
+// @match        *://pastebin.com/*
+// @match        *://paste-drop.com/*
+// @match        *://pastes.io/*
+// @match        *://shorter.me/*
+// @match        *://quartyz.com/*
+// @match        *://rinku-pro.com/*
 // @grant        GM_xmlhttpRequest
 // @grant        GM.xmlHttpRequest
-// @include      *work.ink/*
-// @connect      trw.lat
+// @connect      vortex-bypass-two.vercel.app
 // @run-at       document-start
 // @require      https://github.com/Chaaan0917/Camper2.0/raw/refs/heads/main/Camper.js
 // @updateURL    https://github.com/Chaaan0917/Camper2.0/raw/refs/heads/main/Main2.0.user.js
 // @icon         https://i.kym-cdn.com/entries/icons/original/000/043/403/cover3.jpg
-// @grant        none
 // ==/UserScript==
 
 // set 30 if you are doing luarmor
